ba5dbfe70c33f4fe08c6935cf7304c114045c655
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-31 17:26:00 +0530

    login

--- frontend/app/(auth)/Login.tsx
+++ frontend/app/(auth)/Login.tsx
@@ -1,14 +1,142 @@
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 import ScreenWrapper from "@/components/ScreenWraper";
-import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import * as Icons from "phosphor-react-native";
+import React, { useRef, useState } from "react";
+import {
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  async function handleSubmit(): Promise<void> {
+    if (!emailRef.current || !passwordRef.current) {
+      Alert.alert("Sign Up", "Please fill all the fields");
+      return;
+    }
+    //
+  }
+
   return (
-    <ScreenWrapper>
-      <Text> textInComponent </Text>
-    </ScreenWrapper>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+    >
+      <ScreenWrapper showPattern={true}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <BackButton iconSize={22} />
+            <Typo size={17} color={colors.white}>
+              Need some help?
+            </Typo>
+          </View>
+          <View style={styles.content}>
+            <ScrollView
+              contentContainerStyle={styles.form}
+              showsVerticalScrollIndicator={false}
+            >
+              <View style={{ gap: spacingY._10, marginBottom: spacingY._15 }}>
+                <Typo size={28} fontWeight={"600"}>
+                  Getting started
+                </Typo>
+                <Typo color={colors.neutral600}>
+                  Create an account to continue
+                </Typo>
+              </View>
+
+              <Input
+                placeholder="Enter your Email"
+                onChangeText={(val: string) => (emailRef.current = val)}
+                icon={
+                  <Icons.AtIcon
+                    size={verticalScale(26)}
+                    color={colors.neutral600}
+                  />
+                }
+              />
+              <Input
+                placeholder="Enter your password"
+                secureTextEntry={true}
+                onChangeText={(val: string) => (passwordRef.current = val)}
+                icon={
+                  <Icons.LockIcon
+                    size={verticalScale(26)}
+                    color={colors.neutral600}
+                  />
+                }
+              />
+              <View style={{ marginTop: spacingY._25, gap: spacingY._15 }}>
+                <Button loading={isLoading} onPress={handleSubmit}>
+                  <Typo fontWeight={"bold"} color={colors.black} size={20}>
+                    Sign Up
+                  </Typo>
+                </Button>
+              </View>
+              <View style={styles.footer}>
+                <Typo>have't account?</Typo>
+                <Pressable onPress={() => router.push("/(auth)/Register")}>
+                  <Typo fontWeight={"bold"} color={colors.primaryDark}>
+                    Login
+                  </Typo>
+                </Pressable>
+              </View>
+            </ScrollView>
+          </View>
+        </View>
+      </ScreenWrapper>
+    </KeyboardAvoidingView>
   );
 };
-export default Login;
 
-const styles = StyleSheet.create({});
+export default Login;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // gap: spacingY._30,
+    // marginHorizontal: spacingX._20,
+    justifyContent: "space-between",
+  },
+
+  header: {
+    paddingHorizontal: spacingX._20,
+    paddingTop: spacingY._15,
+    paddingBottom: spacingY._25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    backgroundColor: colors.white,
+    borderTopLeftRadius: radius._50,
+    borderTopRightRadius: radius._50,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._20,
+    paddingTop: spacingY._20,
+  },
+  form: {
+    gap: spacingY._15,
+    marginTop: spacingY._20,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+});
