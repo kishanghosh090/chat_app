6bdc7f5925c2fb3b65b9124c404efebc58d866a3
kishanghosh090 <kishanghosh090@gamil.com>
2025-12-31 19:16:34 +0530

    fix bug

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,10 @@ import "dotenv/config";
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}`);
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_URI}`
+    );
+    console.log(connectionInstance.connection.host);
   } catch (error) {
     console.log(error);
   }
